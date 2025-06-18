cbd92b8edde39d9560023989c15dc321a9839551
Arafat8-8 <99202082+Arafat8-8@users.noreply.github.com>
2025-06-18 02:40:13 -0400

    Initial commit Portfolio website

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css"; 
+
+const Navbar =()=>{
+return(
+<nav className="navbar">
+<div className="navbar-left">
+    Arafat Chowdhury
+</div>
+
+<div className="navbar-right">
+    
+</div>
+</nav>
+)
+}
